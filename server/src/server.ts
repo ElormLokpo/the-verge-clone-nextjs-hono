03d1953cf90281c76b2df9cb70184ce858c616dc
ElormLokpo <benedictdev31@gmail.com>
2026-09-23 04:48:50 +0000

    fix server deploy 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
-import { App, createApp } from "./app";
 import "dotenv/config";
-import { serve } from "@hono/node-server";
+
+import { createApp } from "./app";
 import { env } from "./config";
 
-const app: App = createApp();
-serve({
-    fetch: app.fetch,
-    port: env.PORT
-},
-    () => {
-        console.log(`Server running on PORT:${env.PORT}`);
-    }
-)
+const app = createApp();
+
+console.log(`Server running on PORT:${env.PORT}`);
+
+export default {
+  fetch: app.fetch,
+  port: env.PORT,
+  hostname: "0.0.0.0",
+};
